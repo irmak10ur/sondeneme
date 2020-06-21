f93d6c0f2ae23b705578ad762bdc583615f704d8
irmak10ur <irmak10ur@gmailcom>
2020-06-21 15:47:57 +0200

    Ve bennnn

--- src/gitpackage/GitClass01.java
+++ src/gitpackage/GitClass01.java
@@ -5,6 +5,7 @@ public class GitClass01 {
 	public static void main(String[] args) {
 		System.out.println("Ramazan Irmak");
 		System.out.println("Suheyla Irmak");
+		System.out.println("Onur Irmak");
 		
 		
 		
